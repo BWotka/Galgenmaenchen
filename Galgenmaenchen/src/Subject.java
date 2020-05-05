777ea9bced9d032ba3268c68bcda6fe9ad388b22
bwotka <b.wotka@mail.de>
2020-05-05 17:10:36 +0200

    akt Stand Themen, WordQuiz: Zuständigkeit klären

--- Galgenmaenchen/src/Subject.java
+++ Galgenmaenchen/src/Subject.java
@@ -1,4 +1,4 @@
 
 public enum Subject {
-	Animals(), Cars(), Videogames(), Softdrinks(), Countries(), Cities();
+  Animals(), Cars(), Videogames(), Drinks(), Countries(), Cities();
 }
--- Galgenmaenchen/src/WordQuiz.java
+++ Galgenmaenchen/src/WordQuiz.java
@@ -1,14 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class WordQuiz {
 
   protected ConsoleReader myCReader;
+  protected Writer myExWriter;
   protected Writer myWriter;
+  protected List<WordList> wordLists;
+
+  public WordQuiz() {
+    wordLists = new ArrayList<>();
+
+  }
 
   public boolean addWordList(WordList pWordList) {
+    wordLists.add(pWordList);
     return true;
   }
 
   public boolean deleteWordList(WordList pWordList) {
+    wordLists.remove(pWordList);
     return true;
   }
 
@@ -49,43 +61,93 @@ public class WordQuiz {
   }
 
   public void playGame(int wordLength, Subject wordSubject, Difficulty gameDifficulty) {
-  String gameWord =new WordList(wordSubject).getWordOfLength(wordLength);
-  char[] knownWord = new char[wordLength];
-  char[] triedChars = new char[26];
-  char letter;
-  int errorsLeft = gameDifficulty.getDifficulty();
-  myWriter.setSubject(wordSubject);
-  myWriter.setWord(gameWord);
-  myWriter.setDifficulty(gameDifficulty);
-  
-  
- 
-    //loop in which the turns are made
+    String gameWord = new WordList(wordSubject).getWordOfLength(wordLength);
+    myWriter.setSubject(wordSubject);
+    myWriter.setWord(gameWord);
+    myWriter.setDifficulty(gameDifficulty);
+
+    // the word presented in the console, will be * until the correct letter is found (A-Z, '-')
+    char[] knownWord = new char[wordLength];
+    for (char b : knownWord) {
+      b = '*';
+    }
+
+    // all letters that were tried go in here
+    char[] triedChars = new char[27];
+
+    // current letter
+    char letter;
+
+    int errorsLeft = gameDifficulty.getDifficulty();
+
+
+
+    // loop in which the turns are made
     while (errorsLeft > 0) {
       letter = myCReader.readNextChar();
+
+      // letter is wrong
       if (gameWord.indexOf(letter) == -1) {
-        errorsLeft += -1;
-        triedChars.get;
+
+        // was this letter entered before?
+        boolean letterTriedBefore = false;
+        for (char c : triedChars) {
+          if (c == letter) {
+            letterTriedBefore = true;
+          }
+        }
+        // yes, it was entered before
+        if (letterTriedBefore) {
+          System.out.println("You have entered this letter before");
+        }
+        // no, its a new letter
+        else {
+          System.out.println("This letter is not part of the solution.");
+          errorsLeft += -1;
+          // the letter is added to the list of tried letters
+          for (int c = 0; c < triedChars.length; c++) {
+            if (triedChars[c] == null) {
+              triedChars[c] = letter;
+              c = triedChars.length;
+            }
+          }
+
+        }
+
       }
+      // letter is right
       else {
-        knownWord
+        System.out.println("This letter is part of the solution.");
+
+
+
+        // the letter is added to the list of tried letters
+        for (int c = 0; c < triedChars.length; c++) {
+          if (triedChars[c] == null) {
+            triedChars[c] = letter;
+            c = triedChars.length;
+          }
+        }
       }
       myWriter.write(triedChars, letter, errorsLeft);
-      
-      if (Arrays.equals( knownWord,gameWord.toCharArray())) {
+
+      // if the solution is found out:
+      if (Arrays.equals(knownWord, gameWord.toCharArray())) {
         // The player correctly guessed the word
         System.out.println("You won!");
         System.out.println("You correctly guessed the word: '" + gameWord + "' !");
-        System.out.println("You had " + errorsLeft + " errors left before you would have lost the game!");
+        System.out
+            .println("You had " + errorsLeft + " errors left before you would have lost the game!");
         return;
       }
     }
-  // The player has no tries left, he loses the game
-  System.out.println("Game Over!");
-  System.out.println("The Solution was: "+gameWord);
-  System.out.println("You made "+gameDifficulty.getDifficulty()+"mistakes and lost the game!");
-  
-  
+    // The player has no tries left, he loses the game
+    System.out.println("Game Over!");
+    System.out.println("The Solution was: " + gameWord);
+    System.out
+        .println("You made " + gameDifficulty.getDifficulty() + "mistakes and lost the game!");
+
+
   }
 
 }
