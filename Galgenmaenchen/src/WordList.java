25450724cf346909ceaf51c674ccf59392109c50
bwotka <b.wotka@mail.de>
2020-05-05 15:18:20 +0200

    WordList kann Listen einlesen, nicht getestet

--- Galgenmaenchen/src/WordList.java
+++ Galgenmaenchen/src/WordList.java
@@ -1,21 +1,51 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class WordList {
   private Subject subject;
+  protected List<QuizWord> words;
+  Scanner sc;
 
-  public WordList(Subject psubject) {
-    subject = psubject;
+  public WordList(Subject pSubject) {
+    subject = pSubject;
+    words = new ArrayList<>();
+    if (readListFromFile(pSubject.toString() + ".txt")) {
+      System.out.println("List with the topic '" + subject.toString() + "' was succesfully read.");
+    }
+
   }
 
-  public boolean readListFromFile(String filename) {
+  protected boolean readListFromFile(String fileName) {
+    try {
+      sc = new Scanner(new File(fileName));
+    } catch (Exception e) {
+      return false;
+    }
+    while (sc.hasNextLine()) {
+      words.add(new QuizWord(sc.nextLine()));
+    }
     return true;
   }
 
   public String getRandomWord() {
-    return;
+    return words.get(ThreadLocalRandom.current().nextInt(0, words.size() + 1)).getword();
   }
 
-  public String getWordOfLength(int plength) {
-    return;
+  public String getWordOfLength(int pLength) {
+    List<QuizWord> wordsWithLength = new ArrayList<>();
+
+    // all words with the correct length are placed in a separate list
+    for (QuizWord word : words) {
+      if (word.getlength() == pLength) {
+        wordsWithLength.add(word);
+      }
+    }
+    return wordsWithLength.get(ThreadLocalRandom.current().nextInt(0, wordsWithLength.size() + 1))
+        .getword();
+
   }
 
   public Subject getSubject() {
