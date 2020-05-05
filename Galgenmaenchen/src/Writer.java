65b687e001e9bde112cbf58937bddc6f1679b9aa
dnickel8 <david.nickel@fh-bielefeld.de>
2020-05-05 19:48:33 +0200

    add Writer

--- Galgenmaenchen/src/Writer.java
+++ Galgenmaenchen/src/Writer.java
@@ -1,4 +1,21 @@
 
 public abstract class Writer {
+	protected Subject subject;
+	protected Difficulty difficulty;
+	protected String word;
+	
+	public void setSubject(Subject newSubject) {
+		subject = newSubject;
+	}
+	
+	public void setDifficulty(Difficulty newDifficulty) {
+		difficulty = newDifficulty;
+	}
+	
+	public void setWord(String newWord) {
+		word = newWord;
+	}
+	
+	public abstract boolean write(char[] guessedLetters, char newLetter, int leftFails);
 
 }
