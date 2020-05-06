db5667f7a1f176e1ede9a69205919297d3021a12
dnickel8 <david.nickel@fh-bielefeld.de>
2020-05-06 02:57:06 +0200

    googleStyle in *Writer

--- Galgenmaenchen/src/ConsoleWriter.java
+++ Galgenmaenchen/src/ConsoleWriter.java
@@ -1,39 +1,39 @@
 
 public class ConsoleWriter extends Writer {
 
-	@Override
-	public boolean write(char[] guessedLetters, char newLetter, int leftFails) {
-		System.out.println("verbleibende Fehlversuche: " + leftFails);
-		System.out.println("der geratene Buchstabe ist: " + newLetter);
-		for (int i = 0; i < guessedLetters.length; i++) {
-			System.out.print(guessedLetters[i]);
-		}
-		System.out.println();
-		System.out.println("der geratene Buchstabe ist: " + newLetter);
+  @Override
+  public boolean write(char[] guessedLetters, char newLetter, int leftFails) {
+    System.out.println("verbleibende Fehlversuche: " + leftFails);
+    System.out.println("der geratene Buchstabe ist: " + newLetter);
+    for (int i = 0; i < guessedLetters.length; i++) {
+      System.out.print(guessedLetters[i]);
+    }
+    System.out.println();
+    System.out.println("der geratene Buchstabe ist: " + newLetter);
 
-		boolean guess = false;
-		if (word.indexOf(newLetter) != -1) {
-			guess = true;
-		}
-		for (int i = 0; i < word.length(); i++) {
-			boolean guessed = false;
-			if (word.charAt(i) == '-') {
-				System.out.print(word.charAt(i));
-			} else {
-				for (int j = 0; j < guessedLetters.length; j++) {
-					if (word.charAt(i) == guessedLetters[j]) {
-						System.out.print(word.charAt(i));
-						guessed = true;
-					}
-				}
-				if (!guessed) {
-					System.out.print("_");
-				}
-			}
-		}
-		System.out.println();
+    boolean guess = false;
+    if (word.indexOf(newLetter) != -1) {
+      guess = true;
+    }
+    for (int i = 0; i < word.length(); i++) {
+      boolean guessed = false;
+      if (word.charAt(i) == '-') {
+        System.out.print(word.charAt(i));
+      } else {
+        for (int j = 0; j < guessedLetters.length; j++) {
+          if (word.charAt(i) == guessedLetters[j]) {
+            System.out.print(word.charAt(i));
+            guessed = true;
+          }
+        }
+        if (!guessed) {
+          System.out.print("_");
+        }
+      }
+    }
+    System.out.println();
 
-		return guess;
-	}
+    return guess;
+  }
 
 }
--- Galgenmaenchen/src/ExcelWriter.java
+++ Galgenmaenchen/src/ExcelWriter.java
@@ -3,7 +3,6 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.sql.Timestamp;
 
-import org.apache.poi.hssf.usermodel.HSSFFont;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.hssf.util.HSSFColor;
 import org.apache.poi.ss.usermodel.Cell;
@@ -17,103 +16,103 @@ public class ExcelWriter extends Writer {
 
   @Override
   public boolean write(char[] guessedLetters, char newLetter, int leftFails) {
-	  File file = new File(subject + ".xls");
-	    HSSFWorkbook workbook = new HSSFWorkbook();
-	    Sheet sheet;
+    File file = new File(subject + ".xls");
+    HSSFWorkbook workbook = new HSSFWorkbook();
+    Sheet sheet;
 
 
-	    // make header of .xls
-	    if (!file.exists()) {
-	      // create a new sheet
-	      sheet = workbook.createSheet();
+    // make header of .xls
+    if (!file.exists()) {
+      // create a new sheet
+      sheet = workbook.createSheet();
 
-	      // format header
-	      Font fontHeader = workbook.createFont();
-	      fontHeader.setFontHeightInPoints((short) 11);
-	      fontHeader.setBold(true);
-	      CellStyle cellstyleHeader = workbook.createCellStyle();
-	      cellstyleHeader.setFont(fontHeader);
-	      cellstyleHeader.setFillForegroundColor(HSSFColor.HSSFColorPredefined.YELLOW.getIndex());
-	      cellstyleHeader.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+      // format header
+      Font fontHeader = workbook.createFont();
+      fontHeader.setFontHeightInPoints((short) 11);
+      fontHeader.setBold(true);
+      CellStyle cellstyleHeader = workbook.createCellStyle();
+      cellstyleHeader.setFont(fontHeader);
+      cellstyleHeader.setFillForegroundColor(HSSFColor.HSSFColorPredefined.YELLOW.getIndex());
+      cellstyleHeader.setFillPattern(FillPatternType.SOLID_FOREGROUND);
 
-	      Row header = sheet.createRow(0);
-	      Cell header0 = header.createCell(0);
-	      Cell header1 = header.createCell(1);
-	      Cell header2 = header.createCell(2);
-	      Cell header3 = header.createCell(3);
-	      header0.setCellValue("Zeitstempel");
-	      header1.setCellValue("geratene Buchstaben");
-	      header2.setCellValue("eingegebener Buchstabe");
-	      header3.setCellValue("verbleibende Fehlversuche");
-	      header0.setCellStyle(cellstyleHeader);
-	      header1.setCellStyle(cellstyleHeader);
-	      header2.setCellStyle(cellstyleHeader);
-	      header3.setCellStyle(cellstyleHeader);
-	    } else {
-	      try {
-	        workbook = new HSSFWorkbook(new FileInputStream(file));
-	      } catch (Exception e) {
-	        System.out.println("Error");
-	      }
-	      sheet = workbook.getSheetAt(0);
-	    }
+      Row header = sheet.createRow(0);
+      Cell header0 = header.createCell(0);
+      Cell header1 = header.createCell(1);
+      Cell header2 = header.createCell(2);
+      Cell header3 = header.createCell(3);
+      header0.setCellValue("Zeitstempel");
+      header1.setCellValue("geratene Buchstaben");
+      header2.setCellValue("eingegebener Buchstabe");
+      header3.setCellValue("verbleibende Fehlversuche");
+      header0.setCellStyle(cellstyleHeader);
+      header1.setCellStyle(cellstyleHeader);
+      header2.setCellStyle(cellstyleHeader);
+      header3.setCellStyle(cellstyleHeader);
+    } else {
+      try {
+        workbook = new HSSFWorkbook(new FileInputStream(file));
+      } catch (Exception e) {
+        System.out.println("Error");
+      }
+      sheet = workbook.getSheetAt(0);
+    }
 
-	    // format of .xls
-	    Font font1 = workbook.createFont();
-	    font1.setFontHeightInPoints((short) 11);
+    // format of .xls
+    Font font1 = workbook.createFont();
+    font1.setFontHeightInPoints((short) 11);
 
-	    CellStyle cellstyle1 = workbook.createCellStyle();
-	    CellStyle cellstyle2 = workbook.createCellStyle();
-	    cellstyle1.setFont(font1);
-	    cellstyle2.setFont(font1);
-	    cellstyle1.setFillForegroundColor(HSSFColor.HSSFColorPredefined.GREY_25_PERCENT.getIndex());
-	    cellstyle1.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+    CellStyle cellstyle1 = workbook.createCellStyle();
+    CellStyle cellstyle2 = workbook.createCellStyle();
+    cellstyle1.setFont(font1);
+    cellstyle2.setFont(font1);
+    cellstyle1.setFillForegroundColor(HSSFColor.HSSFColorPredefined.GREY_25_PERCENT.getIndex());
+    cellstyle1.setFillPattern(FillPatternType.SOLID_FOREGROUND);
 
-	    // add row to .xls
-	    Row row = sheet.createRow(sheet.getLastRowNum() + 1);
-	    Cell cell0 = row.createCell(0);
-	    Cell cell1 = row.createCell(1);
-	    Cell cell2 = row.createCell(2);
-	    Cell cell3 = row.createCell(3);
+    // add row to .xls
+    Row row = sheet.createRow(sheet.getLastRowNum() + 1);
+    Cell cell0 = row.createCell(0);
+    Cell cell1 = row.createCell(1);
+    Cell cell2 = row.createCell(2);
+    Cell cell3 = row.createCell(3);
 
-	    Timestamp tstamp = new Timestamp(System.currentTimeMillis());
-	    cell0.setCellValue(tstamp.toString());
-	    String guessedLettersString = new String();
-	    for (char letter : guessedLetters) {
-	      guessedLettersString = guessedLettersString + letter;
-	    }
-	    cell1.setCellValue(guessedLettersString);
-	    cell2.setCellValue(Character.toString(newLetter));
-	    cell3.setCellValue(Integer.toString(leftFails));
+    Timestamp tstamp = new Timestamp(System.currentTimeMillis());
+    cell0.setCellValue(tstamp.toString());
+    String guessedLettersString = new String();
+    for (char letter : guessedLetters) {
+      guessedLettersString = guessedLettersString + letter;
+    }
+    cell1.setCellValue(guessedLettersString);
+    cell2.setCellValue(Character.toString(newLetter));
+    cell3.setCellValue(Integer.toString(leftFails));
 
-	    if (sheet.getLastRowNum() % 2 == 1) {
-	      cell0.setCellStyle(cellstyle1);
-	      cell1.setCellStyle(cellstyle1);
-	      cell2.setCellStyle(cellstyle1);
-	      cell3.setCellStyle(cellstyle1);
-	    } else {
-	      cell0.setCellStyle(cellstyle2);
-	      cell1.setCellStyle(cellstyle2);
-	      cell2.setCellStyle(cellstyle2);
-	      cell3.setCellStyle(cellstyle2);
-	    }
+    if (sheet.getLastRowNum() % 2 == 1) {
+      cell0.setCellStyle(cellstyle1);
+      cell1.setCellStyle(cellstyle1);
+      cell2.setCellStyle(cellstyle1);
+      cell3.setCellStyle(cellstyle1);
+    } else {
+      cell0.setCellStyle(cellstyle2);
+      cell1.setCellStyle(cellstyle2);
+      cell2.setCellStyle(cellstyle2);
+      cell3.setCellStyle(cellstyle2);
+    }
 
-	    sheet.autoSizeColumn(0);
-	    sheet.autoSizeColumn(2);
-	    sheet.autoSizeColumn(3);
-	    sheet.autoSizeColumn(4);
-	    
-	    try {
-	      FileOutputStream output = new FileOutputStream(subject + ".xls");
-	      workbook.write(output);
-	      output.close();
-	      workbook.close();
-	    } catch (Exception e) {
-	      System.out.println("Error");
-	      return false;
-	    }
+    sheet.autoSizeColumn(0);
+    sheet.autoSizeColumn(2);
+    sheet.autoSizeColumn(3);
+    sheet.autoSizeColumn(4);
 
-	    return true;
+    try {
+      FileOutputStream output = new FileOutputStream(subject + ".xls");
+      workbook.write(output);
+      output.close();
+      workbook.close();
+    } catch (Exception e) {
+      System.out.println("Error");
+      return false;
+    }
+
+    return true;
   }
 
 }
