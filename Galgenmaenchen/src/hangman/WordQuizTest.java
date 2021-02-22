0ea43f78bc06f2994804c8a1d0e04ce5c174bdaf
bwotka <b.wotka@mail.de>
2021-02-22 06:32:32 +0100

    summary

---
+++ Galgenmaenchen/src/hangman/WordQuizTest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package hangman;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Benedikt
+ *
+ */
+class WordQuizTest {
+
+  @Test
+  void test() {
+    fail("Not yet implemented");
+  }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Galgenmaenchen
 Gaelgenmaenchen fällig Do 07.05.2020 8:00
+Nötige librarys:
+Apache POI 4.1.2
+commons-math3-3.6.1
